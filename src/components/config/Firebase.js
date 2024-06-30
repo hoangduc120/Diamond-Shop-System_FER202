f3ee88270362d5da83461ca70cc8846450e70433
trunghieungth2004 <trunghieungth2004@gmail.com>
2024-06-30 16:17:17 +0700

    Synced with users API

--- src/components/config/Firebase.js
+++ src/components/config/Firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, sendPasswordResetEmail, signOut } from "firebase/auth";
-import { getFirestore, query, getDocs, collection, where, addDoc } from "firebase/firestore";
+import { getFirestore, query, getDocs, collection, where, addDoc, updateDoc, doc } from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -31,11 +31,22 @@ const signInWithGoogle = async () => {
     const q = query(collection(db, "users"), where("uid", "==", user.uid));
     const docs = await getDocs(q);
     if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
-        uid: user.uid,
-        name: user.displayName,
-        authProvider: "google",
+      const docRef = await addDoc(collection(db, "users"), {
+        user_id: user.uid,
+        fullname: user.displayName,
+        gender: "",
+        avatar: user.photoURL,
         email: user.email,
+        account_status: "",
+        auth: "",
+        authProvider: "google",
+        phone: "",
+        address_shipping: "",
+        role: "",
+      });
+      console.log("Document written with ID: ", docRef.id);
+      await updateDoc(doc(db, "users", docRef.id), {
+        user_id: docRef.id,
       });
     }
   } catch (err) {
@@ -55,11 +66,22 @@ const registerWithEmailAndPassword = async (name, email, password) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
     const user = res.user;
-    await addDoc(collection(db, "users"), {
-      uid: user.uid,
-      name,
+    const docRef = await addDoc(collection(db, "users"), {
+      user_id: user.uid,
+      fullname: name,
+      gender: "",
+      avatar: "",
+      email: "",
+      account_status: "",
+      auth: "",
       authProvider: "local",
-      email,
+      phone: "",
+      address_shipping: "",
+      role: "", 
+    });
+
+    await updateDoc(doc(db, "users", docRef.id), {
+      user_id: docRef.id,
     });
   } catch (err) {
     console.error(err);
@@ -80,3 +102,5 @@ const logout = () => {
   signOut(auth);
 };
 export {auth, db, signInWithGoogle, logInWithEmailAndPassword, registerWithEmailAndPassword, sendPasswordReset, logout};
+
+//TODO: "account_status", "role" is bool
