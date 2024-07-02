84a751cde6a6f6785419d7543f88be5e66010562
Anhtdse183683 <yunct1234@gmail.com>
2024-07-02 14:36:25 +0700

    FAQ, Checkout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import React from 'react';
+import { Container, Typography } from '@mui/material';
+import FAQ from './components/FAQ';
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
-  )
+   
+  );
 }
 
-export default App
+export default App;
