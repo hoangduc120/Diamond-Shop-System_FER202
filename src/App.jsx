599a716e81457de03be7dc7ba2828a0f71a18e39
hoangduc120 <hoangduc05122003@gmail.com>
2024-07-06 17:01:37 +0700

    payment management

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ import HomePage from "./pages/users/homepage/HomePage.jsx";
 import Login from './pages/users/login/Login.jsx'
 import Profile from "./pages/users/profile/Profile.jsx";
 import MyAccount from "./pages/users/profile/MyAccount.jsx";
+import PaymentManagement from "./pages/admin/dashboard/PaymentManagement.jsx";
 
 function App() {
   return (
@@ -38,6 +39,7 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/account" element={<MyAccount />} />
+          <Route path="/paymentmanagement" element={<PaymentManagement />} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/admin/dashboard/PaymentManagement.jsx
@@ -0,0 +1,119 @@
+import {
+  Button,
+  Container,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import axios from "../../../axiosConfig";
+import { useEffect, useState } from "react";
+
+const PaymentManagement = () => {
+  const [users, setUsers] = useState([]);
+  const fetchData = () => {
+    axios
+      .get("/payments")
+      .then((res) => {
+        setUsers(res.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const handleRefund = (payment_id) => {
+    axios
+      .post(`/refund/${payment_id}`, payment_id)
+      .then((res) => {
+        console.log(res.data);
+        fetchData();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <Container maxWidth="lg" style={{ marginTop: "20px" }}>
+      <Typography variant="h5" gutterBottom>
+        Payment Management
+      </Typography>
+      <TableContainer
+        component={Paper}
+        style={{ maxHeight: "60vh", overflowY: "auto" }}
+      >
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>UserID</TableCell>
+              <TableCell>Full Name</TableCell>
+              <TableCell>Payment ID</TableCell>
+              <TableCell>Amount</TableCell>
+              <TableCell>Date</TableCell>
+              <TableCell>Payment Method</TableCell>
+              <TableCell>Status</TableCell>
+              <TableCell>Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {users.length > 0 ? (
+              users.map((user, index) => (
+                <TableRow key={index}>
+                  <TableCell>{user.user_id}</TableCell>
+                  <TableCell>{user.fullname}</TableCell>
+                  <TableCell>{user.payment_id}</TableCell>
+                  <TableCell>{user.totalAmount}</TableCell>
+                  <TableCell>{user.date}</TableCell>
+                  <TableCell>{user.partnerCode}</TableCell>
+                  <TableCell>{user.status ? "True" : "False"}</TableCell>
+                  <TableCell>
+                    <Button
+                      onClick={() => handleRefund(user.payment_id)}
+                      color="primary"
+                      disabled={!user.status}
+                      sx={{
+                        background:
+                          "linear-gradient(45deg, #aaa 30%, #434343 90%)",
+                        border: 0,
+                        borderRadius: 15,
+                        boxShadow: "0 3px 5px 2px rgba(0, 0, 0, .3)",
+                        color: "white",
+                        height: 48,
+                        padding: "0 30px",
+                        fontSize: "1.2rem",
+                        fontWeight: "bold",
+                        textTransform: "none",
+                        "&:hover": {
+                          background:
+                            "linear-gradient(45deg, #434343  30%, #aaa  90%)",
+                        },
+                      }}
+                    >
+                      Refund
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell colSpan={10} align="center">
+                  No users found
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Container>
+  );
+};
+
+export default PaymentManagement;
